c758897014a5bee315432a09d446996119877abb
ben <ben@dreamerslab.com>
2012-02-20 12:53:10 +0800

    add language module to src

---
+++ src/lang.js
@@ -0,0 +1,10 @@
+/*!
+ * WHISKY
+ * Copyright(c) 2011 Ben Lin <ben@dreamerslab.com>
+ * MIT Licensed
+ *
+ * @fileoverview
+ * Instance of $$.Trunk. Place to store language data.
+ */
+
+$$.Lang = new $$.Trunk();
