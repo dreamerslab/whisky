a921128052c2e55d63be6c2d5034e2865beae9c3
ben <ben@dreamerslab.com>
2012-02-20 12:49:37 +0800

    add logger module to src

---
+++ src/logger.js
@@ -0,0 +1,47 @@
+/*!
+ * WHISKY
+ * Copyright(c) 2011 Ben Lin <ben@dreamerslab.com>
+ * MIT Licensed
+ *
+ * @fileoverview
+ * Logger for debugging. Use console.log if avaliable instead of alert.
+ */
+
+/**
+ * Setup Logger module in a closure
+ * @this {window|global}
+ * @param {Object} root The global object, window or global.
+ * @param {Object} $$ WHISKY object.
+ * @param {Object} $ jQuery or Zepto object.
+ */
+( function ( root, $$, $ ){
+  var log = console && console.log ?
+    console.log : alert;
+
+  var json = root.JSON.stringify;
+
+  if( $( '#whisky-config' ).attr( 'env' ) === 'prod' ){
+    log  = function (){};
+    json = function (){};
+  }
+
+/**
+ * Log debugging msg and args.
+ * @this {$$.Event}
+ * @param {String} module Module name.
+ * @param {String} msg A message to be logged.
+ * @param {Object} args An Object to be logged if it exists.
+ * @example
+ *
+ *     $$.Log( 'Trunk', 'Class constructor first argument must a string or an object', {
+ *       key : key,
+ *       val : val
+ *     });
+ */
+  $$.Log = function ( module, msg, args ){
+    var args = args === undefined ?
+      '' : '\nargs: ' + json( args );
+
+    log( '[WHISKY][' + module + ']\nmsg: ' + msg + args );
+  };
+})( this, WHISKY, jQuery || Zepto );
