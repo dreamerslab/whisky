f0660b7684499e1e392189c127ae9f70c69b00dd
ben <ben@dreamerslab.com>
2012-02-20 12:52:20 +0800

    add cache module to src

---
+++ src/cache.js
@@ -0,0 +1,10 @@
+/*!
+ * WHISKY
+ * Copyright(c) 2011 Ben Lin <ben@dreamerslab.com>
+ * MIT Licensed
+ *
+ * @fileoverview
+ * Instance of $$.Trunk for storing cross module data.
+ */
+
+$$.Cache = new $$.Trunk();
