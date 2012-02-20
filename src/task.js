c84d16362965b034046516869e3a5eba989baf5f
ben <ben@dreamerslab.com>
2012-02-20 12:52:50 +0800

    add task module to src

---
+++ src/task.js
@@ -0,0 +1,11 @@
+/*!
+ * WHISKY
+ * Copyright(c) 2011 Ben Lin <ben@dreamerslab.com>
+ * MIT Licensed
+ *
+ * @fileoverview
+ * Instance of $$.Event. Global events as tasks.
+ * Tasks are used as connectors to comunicate cross modules.
+ */
+
+$$.Task = new $$.Event();
