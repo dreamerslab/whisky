529072a5ad8f0803fa9cdb663d4c7f4fe7025621
ben <ben@dreamerslab.com>
2012-03-13 17:04:23 +0800

    update trunk class with new core

--- src/trunk.js
+++ src/trunk.js
@@ -4,21 +4,36 @@
  * MIT Licensed
  *
  * @fileOverview
- * In memory data store class
+ * In memory data store class.
  */
 
-$$.Trunk = $$.Class.extend({
+$$( function ( require, exports ){
+
+/**
+ * Module dependencies.
+ * @private
+ */
+  var Class = require( 'Class' );
+  var is    = require( 'utils' ).is;
+  var log   = require( 'logger' );
+
+/**
+ * Trunk class.
+ * @public
+ * @class
+ */
+  var Trunk = Class.extend({
 
 /**
  * Setup the default values.
  * @constructor
- * @this {$$.Trunk}
+ * @this {Trunk}
  * @param {String|Object} key Data key if arg is sting || Data set if arg is an object.
  * @param {String|Number|Array|Object|Function|null} val Data value.
  * @returns {this} Return `this` to enable chaining.
  * @example
  *
- *     var cache = new $$.Trunk({
+ *     var cache = new Trunk({
  *       age : 17,
  *       name : 'ben',
  *       'do-some-thing' : function( arg1, arg2, arg3 ){
@@ -26,75 +41,78 @@ $$.Trunk = $$.Class.extend({
  *       }
  *     });
  */
-  init : function ( key, val ){
-    var is     = $$.is( key );
-    this.trunk = {};
+    init : function ( name ){
+      this._name = name;
+      this.reset();
 
-    if( is === 'String' ){
-      this.set( key, val );
       return this;
-    }
-
-    if( is === 'Object' ){
-      this.trunk = key;
-      return this;
-    }
-
-    $$.Log( 'Trunk', 'Class constructor first argument must a string or an object', {
-      key : key,
-      val : val
-    });
-  },
+    },
 
 /**
  * Store data in a closure to be called or withdraw later.
- * @this {$$.Trunk}
+ * @public
+ * @this {Trunk}
  * @param {String} key Data key.
  * @param {String|Number|Array|Object|Function} val Data value.
  * @returns {this} Return `this` to enable chaining.
  * @example
  *
- *     var cache = new $$.Trunk();
+ *     var cache = new Trunk();
  *     cache.set( 'age', 17 );
  *     cache.set( 'name', 'ben' );
  *     cache.set( 'do-some-thing', function( arg1, arg2, arg3 ){
  *       // do something here
  *     });
  */
-  set : function( key, val ){
-    // data can be overwritten
-    this.trunk[ key ] = val;
+    set : function( key, val ){
+      if( is( key ) !== 'String' ){
+        return log( this._name + '.set( key, val ) argument `key` must be a string', {
+          key : key,
+          val : val
+        });
+      }
 
-    return this;
-  },
+      // data can be overwritten
+      this.trunk[ key ] = val;
+
+      return this;
+    },
 
 /**
  * Get stored data.
- * @this {$$.Trunk}
+ * @public
+ * @this {Trunk}
  * @param {String} key Data key.
  * @returns {data|false} Return the stored data if available.
  * @example
  *
- *     var cache = new $$.Trunk();
+ *     var cache = new Trunk();
  *     cache.set( 'age', 17 );
  *     // somewhere in another class or function
  *     var age = cache.get( 'age' );
  */
-  get : function( key ){
-    // make sure the calling out name exist
-    return this.trunk[ key ] !== undefined ?
-      this.trunk[ key ] : false;
-  },
+    get : function( key ){
+      if( is( key ) !== 'String' ){
+        return log( this._name + '.get( key ) argument `key` must be a string', {
+          key : key
+        });
+      }
+
+      // make sure the calling out name exist
+      return this.trunk[ key ] !== undefined ?
+        this.trunk[ key ] : false;
+    },
 
 /**
  * Calling a stored function.
- * @this {$$.Trunk}
+ * @public
+ * @this {Trunk}
  * @param {String} arguments[ 0 ] Data key.
  * @param {String|Number|Array|Object|Function} [arguments[ 1 ]] Arguments to be passed to the stored function.
  * @returns {Result} Return the triggerd function result.
  * @example
  *
- *     var cache = new $$.Trunk({
+ *     var cache = new Trunk({
  *       'do-some-thing' : function( arg1, arg2, arg3 ){
  *         // do something here
  *       }
@@ -102,56 +120,92 @@ $$.Trunk = $$.Class.extend({
  *     // You can pass as many arguments as you wish
  *     cache.trigger( 'do-some-thing', arg1, arg2, arg3 ... );
  */
-  trigger : function(){
-    var key = [].shift.call( arguments );
-    // make sure the calling function exist
-    if( this.trunk[ key ] !== undefined ){
+    trigger : function(){
+      var key = [].shift.call( arguments );
+      // make sure the calling function exist
+      if( this.trunk[ key ] !== undefined ){
 
-      // trigger stored function
-      var tmp = this.trunk[ key ];
+        // trigger stored function
+        var tmp = this.trunk[ key ];
 
-      if( $$.is( tmp ) === 'Function' ){
-        return tmp.apply( this, arguments );
-      }else{
-        $$.Log( 'Trunk', 'method `trigger` on - `' + key + '` is not a function' );
+        if( is( tmp ) === 'Function' ){
+          return tmp.apply( this, arguments );
+        }else{
+          return log( this._name + '.trigger() first argument `' + key + '` is not a function', {
+            name : name,
+            fn : fn
+          });
+        }
       }
-    }
-
-    return this;
-  },
+    },
 
 /**
  * Remove stored data.
- * @this {$$.Trunk}
+ * @public
+ * @this {Trunk}
  * @param {String} key Data key.
  * @returns {this} Return `this` to enable chaining.
  * @example
  *
- *     var cache = new $$.Trunk();
+ *     var cache = new Trunk();
  *     cache.set( 'age', 17 );
  *     // somewhere in another class or function
  *     cache.remove( 'age' );
  */
-  remove : function( key ){
-    delete this.trunk[ key ];
+    remove : function( key ){
+      if( is( key ) === 'String' ){
+        delete this.trunk[ key ];
+      }
 
-    return this;
-  },
+      return this;
+    },
 
 /**
  * Reset trunk.
- * @this {$$.Trunk}
+ * @public
+ * @this {Trunk}
  * @returns {this} Return `this` to enable chaining.
  * @example
  *
- *     var cache = new $$.Trunk();
+ *     var cache = new Trunk();
  *     cache.set( 'age', 17 );
  *     // somewhere in another class or function
  *     cache.reset();
  */
-  reset : function (){
-    this.trunk = {};
+    reset : function (){
+      this.trunk = {};
 
-    return this;
-  }
+      return this;
+    },
+
+/**
+ * List trunk items.
+ * @public
+ * @this {Trunk}
+ * @returns {this} Return `this` to enable chaining.
+ * @example
+ *
+ *     var cache = new Trunk();
+ *     cache.set( 'age', 17 );
+ *     // somewhere in another class or function
+ *     cache.reset();
+ */
+    list : function (){
+      var list = [];
+      var name;
+
+      for( name in this.trunk ){
+        list.push( name );
+      }
+
+      log( this._name + 'list()', list );
+
+      return this;
+    }
+  });
+
+/**
+ * Exports module.
+ */
+  exports( 'Trunk', Trunk );
 });
